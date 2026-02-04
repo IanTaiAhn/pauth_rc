fab509574c877993472c5b7d140301d04ed704a9
Ahn, Ian Tai <ian.tai.ahn@lmco.com>
2026-02-04 14:59:32 -0700

    goin back to grab my minilm and reranker

--- backend/app/rag_pipeline/embeddings/embedder.py
+++ backend/app/rag_pipeline/embeddings/embedder.py
@@ -15,7 +15,7 @@ class SentenceTransformerEmbedder(BaseEmbedder):
     def __init__(self, model_name: str = None):
         print('os from embedder.py: ', os.getcwd())
         # Default to local path instead of remote model name
-        self.model_name = model_name or os.getenv("SENT_TRANSFORMER_MODEL", "backend/app/rag_pipeline/models/bge_large_en_v1.5")
+        self.model_name = model_name or os.getenv("SENT_TRANSFORMER_MODEL", "backend/app/rag_pipeline/models/minilm")
         self.model = SentenceTransformer(self.model_name)
 
     def embed(self, texts: List[str]):
